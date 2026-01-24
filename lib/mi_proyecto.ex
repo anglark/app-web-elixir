dfbf4c9862677074ea4d2411ee22c58355eabdf7
anglark <angeleztwilight@gmail.com>
2026-01-24 18:40:49 -0300

    creacion del arhciov de elixir entre otros

---
+++ lib/mi_proyecto.ex
@@ -0,0 +1,18 @@
+defmodule MiProyecto do
+  @moduledoc """
+  Documentation for `MiProyecto`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MiProyecto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
